dd8c2c643352f2e4483c77d9e9632251e81dac6c
Daniel Einor <deino@softserveinc.com>
2021-02-09 22:37:48 +0100

    fix

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import librosa
 
-if librosa.load('TEST18.mp3'):
-    st.success('success')
+mp3 = 'TEST18.mp3'
+loaded = librosa.load(mp3)
+st.sidebar.audio(mp3)
+st.success('success')
